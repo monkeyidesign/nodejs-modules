86b4549fcb094e1cca8621c251e96ec00a48d969
monkeyidesign <sam@monkeyidesign.com>
2021-02-07 23:05:34 +0700

    write data into json file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, resp) => {
     return resp.send('Hello World!')
 })
 
-app.get('/todos', (req, resp) => {
+app.get('/todos/', (req, resp) => {
     fs.readFile('./store/todos.json', 'utf-8', (err, data) => {
         if (err) {
             return resp.status(500).send('Sorry, something went wrong.')
@@ -16,6 +16,37 @@ app.get('/todos', (req, resp) => {
     })
 })
 
+app.put('/todos/:id/complete/', (req, resp) => {
+    const id = req.params.id
+
+    const findTodoById = (todos, id) => {
+        for(let i = 0; i < todos.length; i++){
+            if (todos[i].id === parseInt(id)){
+                return i
+            }
+        }
+        return -1
+    }
+
+    fs.readFile('./store/todos.json', 'utf-8', (err, data) => {
+        if (err) {
+            return resp.status(500).send('Sorry, something went wrong.')
+        }
+        let todos = JSON.parse(data);
+        const todoIndex = findTodoById(todos, id)
+
+        if(todoIndex === -1){
+            return resp.status(404).send("Sorry, not found")
+        }
+        todos[todoIndex].complete = true
+
+        fs.writeFile('./store/todos.json', JSON.stringify(todos), () =>{
+            return resp.json({'status': 'ok'})
+        })
+    })
+})
+
 app.listen(3000, () => {
     console.log('Application is running on http://localhost:3000');
 })
+
